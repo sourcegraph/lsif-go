087b71ddad1cc4758bee0a5026d913740b6d5f37
Eric Fritz <eric@eric-fritz.com>
2020-02-27 17:10:49 -0600

    Remove noisy logs.

--- cmd/lsif-go/main.go
+++ cmd/lsif-go/main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/alecthomas/kingpin"
 	"github.com/sourcegraph/lsif-go/internal/gomod"
 	"github.com/sourcegraph/lsif-go/internal/index"
-	"github.com/sourcegraph/lsif-go/internal/log"
 	"github.com/sourcegraph/lsif-go/protocol"
 )
 
@@ -27,8 +26,6 @@ func main() {
 
 func realMain() error {
 	var (
-		debug          bool
-		verbose        bool
 		projectRoot    string
 		repositoryRoot string
 		moduleVersion  string
@@ -37,8 +34,6 @@ func realMain() error {
 	)
 
 	app := kingpin.New("lsif-go", "lsif-go is an LSIF indexer for Go.").Version(versionString)
-	app.Flag("debug", "Display debug information.").Default("false").BoolVar(&debug)
-	app.Flag("verbose", "Display verbose information.").Short('v').Default("false").BoolVar(&verbose)
 	app.Flag("projectRoot", "Specifies the project root. Defaults to the current working directory.").Default(".").StringVar(&projectRoot)
 	app.Flag("repositoryRoot", "Specifies the repository root.").StringVar(&repositoryRoot)
 	app.Flag("moduleVersion", "Specifies the version of the module defined by this project.").StringVar(&moduleVersion)
@@ -49,17 +44,6 @@ func realMain() error {
 	if err != nil {
 		return err
 	}
-
-	if verbose {
-		log.SetLevel(log.Info)
-	}
-
-	if debug {
-		log.SetLevel(log.Debug)
-	}
-
-	// Print progress dots if we have no other output
-	printProgressDots := !verbose && !debug
 
 	if repositoryRoot == "" {
 		toplevel, err := exec.Command("git", "rev-parse", "--show-toplevel").Output()
@@ -110,24 +94,21 @@ func realMain() error {
 		moduleVersion,
 		dependencies,
 		addContents,
-		printProgressDots,
 		toolInfo,
 		out,
 	)
 
 	start := time.Now()
 	s, err := indexer.Index()
-	if printProgressDots {
-		// End progress line before printing summary or error
-		log.Println()
-		log.Println()
-	}
+
+	// End progress line before printing summary or error
+	fmt.Println()
 
 	if err != nil {
 		return fmt.Errorf("index: %v", err)
 	}
 
-	log.Printf("%d package(s), %d file(s), %d def(s), %d element(s)", s.NumPkgs, s.NumFiles, s.NumDefs, s.NumElements)
-	log.Println("Processed in", time.Since(start))
+	fmt.Printf("%d package(s), %d file(s), %d def(s), %d element(s)", s.NumPkgs, s.NumFiles, s.NumDefs, s.NumElements)
+	fmt.Println("Processed in", time.Since(start))
 	return nil
 }
