b567af5fe14079dd0f183d5802eb8a18fe4d6f4a
Eric Fritz <eric@eric-fritz.com>
2020-09-02 16:40:51 -0500

    Help out diff.

---
+++ cmd/lsif-go/index.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/sourcegraph/lsif-go/internal/indexer"
+	"github.com/sourcegraph/lsif-go/internal/writer"
+	"github.com/sourcegraph/lsif-go/protocol"
+)
+
+func writeIndex(repositoryRoot, projectRoot, moduleName, moduleVersion string, dependencies map[string]string, outFile string) error {
+	start := time.Now()
+
+	out, err := os.Create(outFile)
+	if err != nil {
+		return fmt.Errorf("create dump file: %v", err)
+	}
+	defer out.Close()
+
+	toolInfo := protocol.ToolInfo{
+		Name:    "lsif-go",
+		Version: version,
+		Args:    os.Args[1:],
+	}
+
+	packageDataCache := indexer.NewPackageDataCache()
+
+	// TODO(efritz) - With cgo enabled, the indexer cannot handle packages
+	// that include assembly (.s) files. To index such a package you need to
+	// set CGO_ENABLED=0. Consider maybe doing this explicitly, always.
+	indexer := indexer.New(
+		repositoryRoot,
+		projectRoot,
+		toolInfo,
+		moduleName,
+		moduleVersion,
+		dependencies,
+		writer.NewJSONWriter(out),
+		packageDataCache,
+		!noProgress,
+		noOutput,
+		verboseOutput,
+	)
+
+	if err := indexer.Index(); err != nil {
+		return fmt.Errorf("index: %v", err)
+	}
+
+	if !noOutput && verboseOutput {
+		displayStats(indexer.Stats(), packageDataCache.Stats(), start)
+	}
+
+	return nil
+}
--- cmd/lsif-go/main.go
+++ cmd/lsif-go/main.go
@@ -4,14 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/sourcegraph/lsif-go/internal/gomod"
-	"github.com/sourcegraph/lsif-go/internal/indexer"
-	"github.com/sourcegraph/lsif-go/internal/util"
-	"github.com/sourcegraph/lsif-go/internal/writer"
-	"github.com/sourcegraph/lsif-go/protocol"
 )
 
 func init() {
@@ -37,72 +31,5 @@ func mainErr() error {
 		return err
 	}
 
-	start := time.Now()
-
-	out, err := os.Create(outFile)
-	if err != nil {
-		return fmt.Errorf("create dump file: %v", err)
-	}
-	defer out.Close()
-
-	toolInfo := protocol.ToolInfo{
-		Name:    "lsif-go",
-		Version: version,
-		Args:    os.Args[1:],
-	}
-
-	packageDataCache := indexer.NewPackageDataCache()
-
-	// TODO(efritz) - With cgo enabled, the indexer cannot handle packages
-	// that include assembly (.s) files. To index such a package you need to
-	// set CGO_ENABLED=0. Consider maybe doing this explicitly, always.
-	indexer := indexer.New(
-		repositoryRoot,
-		projectRoot,
-		toolInfo,
-		moduleName,
-		moduleVersion,
-		dependencies,
-		writer.NewJSONWriter(out),
-		packageDataCache,
-		!noProgress,
-		noOutput,
-		verboseOutput,
-	)
-
-	if err := indexer.Index(); err != nil {
-		return fmt.Errorf("index: %v", err)
-	}
-
-	if !noOutput && verboseOutput {
-		indexerStats := indexer.Stats()
-		packageDataCacheStats := packageDataCache.Stats()
-
-		stats := []struct {
-			name  string
-			value string
-		}{
-			{"Wall time elapsed", fmt.Sprintf("%s", util.HumanElapsed(start))},
-			{"Packages indexed", fmt.Sprintf("%d", indexerStats.NumPkgs)},
-			{"Files indexed", fmt.Sprintf("%d", indexerStats.NumFiles)},
-			{"Definitions indexed", fmt.Sprintf("%d", indexerStats.NumDefs)},
-			{"Elements emitted", fmt.Sprintf("%d", indexerStats.NumElements)},
-			{"Packages traversed", fmt.Sprintf("%d", packageDataCacheStats.NumPks)},
-		}
-
-		n := 0
-		for _, stat := range stats {
-			if n < len(stat.name) {
-				n = len(stat.name)
-			}
-		}
-
-		fmt.Printf("\nStats:\n")
-
-		for _, stat := range stats {
-			fmt.Printf("\t%s: %s%s\n", stat.name, strings.Repeat(" ", n-len(stat.name)), stat.value)
-		}
-	}
-
-	return nil
+	return writeIndex(repositoryRoot, projectRoot, moduleName, moduleVersion, dependencies, outFile)
 }
