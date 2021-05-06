b2220b48a3508f91de39399a617d9f047eb89a17
Eric Fritz <eric@eric-fritz.com>
2021-05-06 14:39:30 -0500

    Squash commits.

--- cmd/lsif-go/main.go
+++ cmd/lsif-go/main.go
@@ -26,7 +26,12 @@ func mainErr() error {
 		return err
 	}
 
-	moduleName, dependencies, err := gomod.ListModules(moduleRoot)
+	moduleName, err := gomod.ModuleName(moduleRoot, repositoryRemote)
+	if err != nil {
+		return err
+	}
+
+	dependencies, err := gomod.ListDependencies(moduleRoot, moduleName, moduleVersion)
 	if err != nil {
 		return err
 	}
