c454553182f4d3d0a89f873ff86ed398a541fcac
Eric Fritz <eric@eric-fritz.com>
2021-05-20 10:01:11 -0500

    Add additional testdata.

--- internal/testdata/composite.go
+++ internal/testdata/composite.go
@@ -1,5 +1,7 @@
 package testdata
 
+import "fmt"
+
 type Inner struct {
 	X int
 	Y int
@@ -9,4 +11,17 @@ type Inner struct {
 type Outer struct {
 	Inner
 	W int
+}
+
+func useOfCompositeStructs() {
+	o := Outer{
+		Inner: Inner{
+			X: 1,
+			Y: 2,
+			Z: 3,
+		},
+		W: 4,
+	}
+
+	fmt.Printf("> %d\n", o.X)
 }
